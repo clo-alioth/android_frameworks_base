b695b44b678932f5c879a67e7f7c5f0186dbff91
Chirayu Desai <chirayudesai1@gmail.com>
2019-12-06 19:22:09 +0530

    SettingsProvider: Set Seedvault as the default backup provider
    Change-Id: Ifa5426d9a6958109b3c6aff87a4cbc4c2c83e8ef
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -3631,7 +3631,7 @@ public class SettingsProvider extends ContentProvider {
         }
 
         private final class UpgradeController {
-            private static final int SETTINGS_VERSION = 210;
+            private static final int SETTINGS_VERSION = 211;
 
             private final int mUserId;
 
@@ -5522,6 +5522,34 @@ public class SettingsProvider extends ContentProvider {
                     // removed now that feature is enabled for everyone
                     currentVersion = 210;
                 }
+
+                if (currentVersion == 210) {
+                    // Version 210: Update default Backup app to Seedvault
+                    final SettingsState secureSettings = getSecureSettingsLocked(userId);
+                    Setting currentBackupTransportSetting = secureSettings.getSettingLocked(
+					                            Secure.BACKUP_TRANSPORT);
+                    if (currentBackupTransportSetting.isDefaultFromSystem()) {
+                        secureSettings.insertSettingLocked(
+                                Settings.Secure.BACKUP_TRANSPORT,
+                                getContext().getResources().getString(
+                                        R.string.def_backup_transport),
+                                	null, true,
+                                SettingsState.SYSTEM_PACKAGE_NAME);
+                    }
+
+                    Setting currentBackupEnabledSetting = secureSettings.getSettingLocked(
+                            						Secure.BACKUP_ENABLED);
+                    if (currentBackupEnabledSetting.isDefaultFromSystem()) {
+                        secureSettings.insertSettingLocked(
+                                Settings.Secure.BACKUP_ENABLED,
+                                getContext().getResources().getBoolean(
+                                        R.bool.def_backup_enabled)? "1" : "0",
+                                	null, true,
+                                SettingsState.SYSTEM_PACKAGE_NAME);
+                    }
+                    currentVersion = 211;
+                }
+
 
                 // vXXX: Add new settings above this point.
 
